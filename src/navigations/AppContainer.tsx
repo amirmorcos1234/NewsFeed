8dae5df134fc2fe665eaea4097d68292954f4969
amirmorcos12345 <100850836+amirmorcos12345@users.noreply.github.com>
2022-06-17 23:52:04 +0200

    feat: add navigation

---
+++ src/navigations/AppContainer.tsx
@@ -0,0 +1,14 @@
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { View } from "react-native";
+
+
+const AppContainer=()=>{
+return (
+    <NavigationContainer>
+        <View/>
+        </NavigationContainer>
+)
+}
+
+export default AppContainer
